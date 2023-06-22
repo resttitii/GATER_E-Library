b7aae27fba790fd1c03f2d45eb772022274222f4
t i t i <212410103005@mail.unej.ac.id>
2023-06-22 19:18:18 +0700

    Add files via upload

---
+++ PBOProjectAkhir/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace PBOProjectAkhir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBuku = new System.Windows.Forms.TextBox();
+            this.txtPengarang = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKategori = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSelect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvData
+            // 
+            this.dgvData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvData.BackgroundColor = System.Drawing.Color.White;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(55, 290);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.ReadOnly = true;
+            this.dgvData.RowHeadersWidth = 62;
+            this.dgvData.RowTemplate.Height = 28;
+            this.dgvData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvData.Size = new System.Drawing.Size(1193, 333);
+            this.dgvData.TabIndex = 0;
+            this.dgvData.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvData_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 211);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Buku";
+            // 
+            // txtBuku
+            // 
+            this.txtBuku.BackColor = System.Drawing.Color.White;
+            this.txtBuku.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBuku.Location = new System.Drawing.Point(110, 209);
+            this.txtBuku.Name = "txtBuku";
+            this.txtBuku.Size = new System.Drawing.Size(153, 29);
+            this.txtBuku.TabIndex = 1;
+            // 
+            // txtPengarang
+            // 
+            this.txtPengarang.BackColor = System.Drawing.Color.White;
+            this.txtPengarang.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPengarang.Location = new System.Drawing.Point(396, 209);
+            this.txtPengarang.Name = "txtPengarang";
+            this.txtPengarang.Size = new System.Drawing.Size(153, 29);
+            this.txtPengarang.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(289, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pengarang";
+            // 
+            // txtKategori
+            // 
+            this.txtKategori.BackColor = System.Drawing.Color.White;
+            this.txtKategori.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKategori.Location = new System.Drawing.Point(663, 209);
+            this.txtKategori.Name = "txtKategori";
+            this.txtKategori.Size = new System.Drawing.Size(153, 29);
+            this.txtKategori.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(574, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Kategori";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInsert.BackColor = System.Drawing.Color.Teal;
+            this.btnInsert.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.ForeColor = System.Drawing.Color.White;
+            this.btnInsert.Location = new System.Drawing.Point(876, 200);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(120, 46);
+            this.btnInsert.TabIndex = 4;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = false;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpdate.BackColor = System.Drawing.Color.Teal;
+            this.btnUpdate.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.ForeColor = System.Drawing.Color.White;
+            this.btnUpdate.Location = new System.Drawing.Point(1002, 200);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(120, 46);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.BackColor = System.Drawing.Color.Teal;
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Location = new System.Drawing.Point(1128, 200);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(120, 46);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(27, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(215, 38);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Gater E-Library";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.ForeColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1311, 150);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelect.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelect.Location = new System.Drawing.Point(1101, 650);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(147, 43);
+            this.btnSelect.TabIndex = 7;
+            this.btnSelect.Text = "LoadData";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(1311, 719);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.txtKategori);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPengarang);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBuku);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvData);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gater E-Library";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBuku;
+        private System.Windows.Forms.TextBox txtPengarang;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKategori;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSelect;
+    }
+}
+
---
+++ PBOProjectAkhir/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql; // untuk mengenali class Npgsqlconnection =>>> Dbconnection
+
+namespace PBOProjectAkhir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private NpgsqlConnection conn; // untuk menghubungkan dengan database
+        //connectionstring => server (alamat database server), port (port database server),
+        //user id (username database yang digunakan untuk terhubung ke database), password (password dari username),
+        //database (nama database yang akan dipakai)
+        string connstring = String.Format("Server = {0}; Port={1};" +
+                        "User Id={2}; Password={3}; Database={4};", "localhost", "5432", "postgres", 
+                        "@postgre16teai", "database_projectakhir_gaterelibrary");
+        private DataTable dt; // untuk menyimpan data dari database dalam bentuk tabel
+        private NpgsqlCommand cmd; // untuk menjalankan query dalam database
+        private string sql = null;
+
+        private DataGridViewRow r;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new NpgsqlConnection(connstring); // ConnectionString ke dalam Constructor class Npgsqlconnection
+            btnSelect.PerformClick();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dgvData.DataSource = null;
+                conn.Open(); // membuka koneksi ke database server
+                sql = "select * from bk_select()";
+                cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                dgvData.DataSource = dt;
+                conn.Close(); // menutup koneksi ke database server
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Fail!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                conn.Close(); // menutup koneksi ke database server
+            }
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                sql = @"select * from bk_insert(:_buku, :_pengarang, :_kategori)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_buku", txtBuku.Text);
+                cmd.Parameters.AddWithValue("_pengarang", txtPengarang.Text);
+                cmd.Parameters.AddWithValue("_kategori", txtKategori.Text);
+                if ((int) cmd.ExecuteScalar() == 1)
+                {
+                    conn.Close();
+                    MessageBox.Show("Data buku yang dimasukkan berhasil!", "Sukses!", MessageBoxButtons.OK, 
+                                    MessageBoxIcon.Information);
+                    btnSelect.PerformClick();
+                    txtBuku.Text = txtKategori.Text = txtPengarang.Text = null;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Dgagal dimasukkan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                conn.Close(); // menutup koneksi ke database server
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if(r == null)
+            {
+                MessageBox.Show("tolong pilih murid yang akan diupdate", "opppp", MessageBoxButtons.OK, 
+                                   MessageBoxIcon.Warning);
+                return;
+            }
+            try
+            {
+                conn.Open();
+                sql = @"select * from bk_update(:_id, :_buku, :_pengarang, :_kategori)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_id", r.Cells["_id"].Value.ToString());
+                cmd.Parameters.AddWithValue("_buku", txtBuku.Text);
+                cmd.Parameters.AddWithValue("_pengarang", txtPengarang.Text);
+                cmd.Parameters.AddWithValue("_kategori", txtKategori.Text);
+                if ((int)cmd.ExecuteScalar() == 1)
+                {
+                    conn.Close(); // menutup koneksi ke database server
+                    MessageBox.Show("update berhasil", "bagus", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    btnSelect.PerformClick();
+                    txtBuku.Text = txtKategori.Text = txtPengarang.Text = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "update gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                conn.Close(); // menutup koneksi ke database server
+            }
+        }
+
+        private void dgvData_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex >= 0)
+            {
+                r = dgvData.Rows[e.RowIndex];
+                txtBuku.Text = r.Cells["_buku"].Value.ToString();
+                txtPengarang.Text = r.Cells["_pengarang"].Value.ToString();
+                txtKategori.Text = r.Cells["_kategori"].Value.ToString();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (r == null)
+            {
+                MessageBox.Show("pilih yang mo diapus", "opps", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if(MessageBox.Show("kamu yakin mo apus? ["+r.Cells["_kategori"].Value.ToString()+"]??", "konfirmasi hapus", 
+                                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                try
+                {
+                    conn.Open();
+                    sql = @"select * from bk_delete(:_id)";
+                    cmd = new NpgsqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("_id", r.Cells["_id"].Value.ToString());
+                    if((int)cmd.ExecuteScalar() == 1)
+                    {
+                        conn.Close();
+                        MessageBox.Show("delete berhasil", "oke berhasil", MessageBoxButtons.OK, 
+                                        MessageBoxIcon.Information);
+                        btnSelect.PerformClick();
+                        txtBuku.Text = txtKategori.Text = txtPengarang.Text = null;
+                        r = null;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    conn.Close();
+                    MessageBox.Show("error: " + ex.Message, "delete gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
